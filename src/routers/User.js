b48272a7212bf83738a6131ab857a6ef688fc1ca
HoangMinh456 <xinphepgiauten456@gmail.com>
2025-10-25 11:41:31 +0700

    add route

--- src/routers/User.js
+++ src/routers/User.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { ChangeAccountName, SignIn, SignUp } from '../controllers/User.js';
+import { ChangeAccountName, ChangePassword, SignIn, SignUp } from '../controllers/User.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/signup', SignUp);
 userRouter.post('/signin', SignIn);
 userRouter.post('/changeaccountname', ChangeAccountName);
+userRouter.post('/changepassword', ChangePassword);
 
 export default userRouter
